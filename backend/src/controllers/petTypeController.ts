7a8d8d03501eb075fbe0d9791b57ee3444fb519a
Ravi Ranjan Kumar <raviranjankai0@gmail.com>
2025-05-01 15:37:34 +0530

    FIX: added new keywords and sending object instead of different parameters

--- backend/src/controllers/petTypeController.ts
+++ backend/src/controllers/petTypeController.ts
@@ -5,18 +5,18 @@ import { AppError } from "../utils/AppError";
 
 export const createPetType = async (req: Request, res: Response) => {
   const petType = await PetType.create(req.body);
-  res.status(201).json(ApiResponse("Pet type created", petType));
+  res.status(201).json(new ApiResponse({ message: "Pet type created", data: petType }));
 };
 
 export const getAllPetTypes = async (_req: Request, res: Response) => {
   const petTypes = await PetType.find();
-  res.status(200).json(ApiResponse("Pet types fetched", petType));
+  res.status(200).json(new ApiResponse({ message: "Pet types fetched", data: petTypes }));
 };
 
 export const deletePetType = async (req: Request, res: Response) => {
   const petType = await PetType.findByIdAndDelete(req.params.id);
-  if (!petType) return next(new AppError("Pet type not found", 404));
-  res.status().json(ApiResponse("Pet type deleted"));
+  if (!petType) return new AppError("Pet type not found", 404);
+  res.status(404).json(new ApiResponse({ message: "Pet type deleted" }));
 };
 
 // export const updatePetType = async (req: Request, res: Response) => {
